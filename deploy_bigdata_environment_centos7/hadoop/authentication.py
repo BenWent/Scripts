8d5703404bed956c158669479d8dcb0269051a37
ben <2311547069@qq.com>
2019-07-14 19:26:59 +0800

    optimize code for authenticating each other in clusters run once in one machine in cluster

--- deploy_bigdata_environment_centos7/hadoop/authentication.py
+++ deploy_bigdata_environment_centos7/hadoop/authentication.py
@@ -2,12 +2,24 @@
 # Description:在集群之间配置SSH互信，使集群中的机器可以互相免密码登录
 
 import os
+import paramiko
+import getpass
+import crypt
+import random
+import time
 
-# 生成密钥
-os.system('ssh-keygen -t rsa')
-# 关闭防火墙
-os.system('systemctl stop firewalld')
+# 以该用户名作为互信登录的用户名
+# user = 'hadoop'
+user = input('new user:')
+# 以该密码作为互信登录用户的密码
+# password = '123456'
+password = getpass.getpass('new user password:')
+encrypted_password = crypt.crypt(password, str(random.randint(0, 9999)))
 
+# root用户密码
+root_password = getpass.getpass('root password:')
+
+# 新建用户并生成密钥
 with open('/etc/hosts', mode='r') as file:
     for line in file:
         ip_name = line.strip().split()
@@ -16,5 +28,98 @@ with open('/etc/hosts', mode='r') as file:
             continue
 
         ip = ip_name[0]
-        # 配置SSH互信
-        os.system("ssh-copy-id -i ~/.ssh/id_rsa.pub hadoop@%s" % ip)
+
+        client = paramiko.SSHClient()
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        # 以root用户登录（创建新用户，关闭防火墙）
+        client.connect(hostname=ip, port=22, username='root',
+                       password=root_password)
+
+        # 获得user用户唯一标识符
+        _, stdout, _ = client.exec_command('id -u {user}'.format(user=user))
+        stdout.channel.recv_exit_status()
+        user_id = stdout.readline()
+        if not user_id.isdigit():  # user用户不存在，才创建新的用户
+            # -r指定创建系统用户，-m指定创建home目录
+            _, stdout, _ = client.exec_command(
+                'useradd -r -m -s /bin/bash -p {password} {user}'.format(user=user, password=encrypted_password))
+            stdout.channel.recv_exit_status()
+
+        # 生成密钥
+        try:
+            sftp = client.open_sftp()
+            sftp.stat('/home/{user}/.ssh/id_rsa'.format(user=user))
+        except IOError:  # user用户没有执行过ssh-keygen命令
+            # -f指定将生成的密钥放到哪个文件，-N，指定 password phrase
+            client.exec_command('mkdir -p ~/.ssh')
+            _, stdout, _ = client.exec_command(
+                'su {user} -c "ssh-keygen -t rsa -f ~/.ssh/id_rsa -N {phrase}"\n'.format(user=user, phrase="\'\'"))
+            stdout.channel.recv_exit_status()
+
+            print(stdout.read())
+
+        # 关闭防火墙
+        _, stdout, _ = client.exec_command('systemctl stop firewalld')
+        stdout.channel.recv_exit_status()
+
+        # 关闭连接
+        client.close()
+
+# 节点机器间SSH互信
+with open('/etc/hosts', mode='r') as file:
+    for line in file:
+        ip_name = line.strip().split()
+
+        if len(ip_name) != 2:
+            continue
+
+        ip = ip_name[0]
+
+        client = paramiko.SSHClient()
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        # 以root用户登录
+        client.connect(hostname=ip, port=22, username=user,
+                       password=password)
+
+        with open('/etc/hosts', mode='r') as _file:
+            for _line in _file:
+                _ip_name = _line.strip().split()
+
+                if len(_ip_name) != 2:
+                    continue
+
+                _ip = _ip_name[0]
+
+                # 配置SSH互信
+                channel = client.invoke_shell()
+                channel.send(
+                    "ssh-copy-id -i ~/.ssh/id_rsa.pub {user}@{ip}\n".format(user=user, ip=_ip))
+                while not channel.recv_ready():
+                    time.sleep(0.1)
+
+                # 等待对方发送完全的信息
+                time.sleep(2)
+                resp = channel.recv(9999)
+                if resp.find('?') != -1:  # ip指定的机器没有加入到  ~/.ssh/known_hosts 文件中
+                    channel.send('yes\n')
+                    while not channel.recv_ready:
+                        time.sleep(0.1)
+
+                    time.sleep(2)
+                    resp = channel.recv(9999)
+                # print(resp)
+                if resp.find('password:') != -1:  # 如果出现了 password:，需要输入该台机器的密码
+                    channel.send('%s\n' % password)
+                    while not channel.recv_ready():  # 确保的密码输入
+                        time.sleep(0.1)
+
+                time.sleep(2)
+        client.close()
+
+
+# 参考
+# 1、Executing ssh-keygen automatically with Paramiko：
+# https://stackoverflow.com/questions/55672657/executing-ssh-keygen-automatically-with-paramiko
+# 2、Executing su user (without password) on paramiko ssh
+# connection：https://stackoverflow.com/questions/8418026/executing-su-user-without-password-on-paramiko-ssh-connection
+# 3、example_paramiko_with_tty.py：https://gist.github.com/rtomaszewski/3397251
