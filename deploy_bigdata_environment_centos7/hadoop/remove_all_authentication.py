9b9592292db9955416def0a2872c35543fbe701c
ben <2311547069@qq.com>
2019-07-15 10:17:20 +0800

    modify the annotation for file hadoop/remove_all_authentication.py

--- deploy_bigdata_environment_centos7/hadoop/remove_all_authentication.py
+++ deploy_bigdata_environment_centos7/hadoop/remove_all_authentication.py
@@ -3,19 +3,17 @@
 # @Author: ben
 # @Date:   2019-07-14 18:33:43
 # @Last Modified by:   ben
-# @Last Modified time: 2019-07-14 19:21:49
-# @Description: 	   移除所以集群指定用户以及该用户间的免密码登录
+# @Last Modified time: 2019-07-15 10:04:55
+# @Description: 	   移除集群中的指定用户
 
 import paramiko
 import getpass
-
 
 # 待移除的用户
 user = input('user:')
 # root用户密码
 root_password = getpass.getpass('root password:')
 
-# 新建用户并生成密钥
 with open('/etc/hosts', mode='r') as file:
     for line in file:
         ip_name = line.strip().split()
@@ -27,7 +25,7 @@ with open('/etc/hosts', mode='r') as file:
 
         client = paramiko.SSHClient()
         client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        # 以root用户登录（创建新用户，关闭防火墙）
+        # 以root用户登录
         client.connect(hostname=ip, port=22, username='root',
                        password=root_password)
 
